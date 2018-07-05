2044223e0d9dfe9130b6d69a85924321d60dbd7a
Florian <fbronnimann@wizcorp.jp>
2018-07-05 18:25:38 +0900

    Removed useless files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const PIXI = require();
-const Game_1 = require("./base/Game");
-window.PIXI = PIXI;
-const game = new Game_1.Game();
-//# sourceMappingURL=index.js.map
